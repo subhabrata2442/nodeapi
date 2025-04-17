4db4c40cc460a1a465c7aefc6e56b7e9932d69a7
subhabrata2442 <subhabrata2442@gmail.com>
2025-04-17 15:55:54 +0530

    comment

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -2,6 +2,7 @@ import { Router } from "express";
 import { getUsers } from "../controllers/user.controller.js";
 const router = Router();
 
+// Get User List
 router.get("/list", async (req, res, next) => {
   res.return(
     await getUsers({
